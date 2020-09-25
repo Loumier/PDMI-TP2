62525acf49644792ac060fedb671e1a48dc96bee
Guilherme <loumier28@gmail.com>
2020-09-24 23:52:38 -0300

    Add project files.

---
+++ tp2/tp2/Contact.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace tp2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Contact : ContentPage
+    {
+        public Contact()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ tp2/tp2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace tp2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void goToStandardStyle(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new StandardStyle());
+        }
+
+        private async void goToDynamicStyle(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new DynamicStyle());
+        }
+
+        private async void goToEventTriggers(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new EventTriggers());
+        }
+        
+        private async void goToSimpleListView(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SimpleListView());
+        }
+
+        private async void goToHome(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Home());
+        }
+    }
+}
